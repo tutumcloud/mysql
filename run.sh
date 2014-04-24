120136870283eb2c419e2f7a40724962ec0f78c3
hfeng <tifayuki@gmail.com>
2014-04-23 20:39:29 -0400

    add env var to skip admin user creation

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ -n "$MYSQL_ADMIN_CREATED" ]; then
+	touch /.mysql_admin_created 
+fi
+
 if [ ! -f /.mysql_admin_created ]; then
 	/create_mysql_admin_user.sh
 fi
+
 exec supervisord -n
