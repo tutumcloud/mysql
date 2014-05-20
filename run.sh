79a67c4e70b1ddd2de8fc971fc123c9e066a9c23
tifayuki <tifayuki@gmail.com>
2014-05-20 11:10:22 -0400

    remove folder operations

--- run.sh
+++ run.sh
@@ -5,11 +5,9 @@ VOLUME_HOME="/var/lib/mysql"
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
-    mkdir -p $VOLUME_HOME
-    chown mysql:mysql -R $VOLUME_HOME
     mysql_install_db > /dev/null 2>&1
     echo "=> Done!"  
-	/create_mysql_admin_user.sh
+    /create_mysql_admin_user.sh
 else
     echo "=> Using an existing volume of MySQL"
 fi
