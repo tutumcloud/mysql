8bc32e8c3a25d8c93deddba4a6fc9c017226861d
tifayuki <tifayuki@gmail.com>
2014-11-04 12:32:23 +0000

    fix #39

--- 5.6/run.sh
+++ 5.6/run.sh
@@ -2,6 +2,7 @@
 
 VOLUME_HOME="/var/lib/mysql"
 CONF_FILE="/etc/mysql/conf.d/my.cnf"
+LOG="/var/log/mysql/error.log"
 
 # Set permission of config file
 chmod 644 ${CONF_FILE}
@@ -10,12 +11,18 @@ chmod 644 /etc/mysql/conf.d/mysqld_charset.cnf
 StartMySQL ()
 {
     /usr/bin/mysqld_safe > /dev/null 2>&1 &
-    RET=1
-    while [[ RET -ne 0 ]]; do
-        echo "=> Waiting for confirmation of MySQL service startup"
+
+    # Time out in 1 minute
+    LOOP_LIMIT=13
+    for (( i=0 ; ; i++ )); do
+        if [ ${i} -eq ${LOOP_LIMIT} ]; then
+            echo "Time out. Error log is shown as below:"
+            tail -n 100 ${LOG}
+            exit 1
+        fi
+        echo "=> Waiting for confirmation of MySQL service startup, trying ${i}/${LOOP_LIMIT} ..."
         sleep 5
-        mysql -uroot -e "status" > /dev/null 2>&1
-        RET=$?
+        mysql -uroot -e "status" > /dev/null 2>&1 && break
     done
 }
 
